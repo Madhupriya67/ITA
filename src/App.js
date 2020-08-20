896c70e152d735c62a672529e08ba57744963a9a
Madhupriya67 <madhubendili67@gmail.com>
2020-08-20 14:34:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ITAComponent from './ITAComponent';
+function App() {
+  return (
+    <div className="App">
+     <ITAComponent/>
+    </div>
+  );
+}
+
+export default App;
